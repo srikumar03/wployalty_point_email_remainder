1adb7004c5834c26f11e606f37165444c5f1dc4b
SRIDHAR KUMAR <96507579+srikumar03@users.noreply.github.com>
2024-10-22 15:46:29 +0530

    feat: Add WP Loyalty Point Email Reminder plugin
    Adds the basic structure and essential files for the WP Loyalty Point Email Reminder plugin, including plugin metadata and a basic plugin file.

---
+++ wployalty_point_email_remainder.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: wployalty_point_email_remainder
+ * Plugin URI: https://www.flycart.org
+ * Description: This add-on allows check license for J2Store plugins
+ * Version: 1.0.0
+ * Author: Sridhar
+ * Slug: Subscription Email Reminder
+ * Text Domain:Subscription Email Reminder
+ * Domain Path: /i18n/languages/
+ * Requires at least: 4.9.0
+ * Author URI:
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
